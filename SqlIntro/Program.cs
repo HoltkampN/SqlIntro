fdda71dbc315643041ad66a135f44d56fb1120d3
Noah Holtkamp <noah.holtkamp@gmail.com>
2017-10-26 13:39:26 -0700

    commits

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
 
 namespace SqlIntro
 {
@@ -6,35 +11,42 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=1234;"; //get connectionString format from connectionstrings.com and change to match your database
-            var repo = new DapperDb(connectionString);
-            foreach (var prod in repo.GetProducts())
-            {
-                Console.WriteLine("Product Name: " + prod.Name + " Time: " + prod.Date.DayOfWeek);
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
 
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                var repo = new DapperDb(conn);
+
+                foreach (var prod in repo.GetProducts())
+                {
+                    Console.WriteLine("Product Name: " + prod.Name + " Time: " + prod.Date.DayOfWeek);
+
+                }
+
+                repo.DeleteProduct(500);
+
+                repo.InsertProduct(new Product
+                {
+                    Name = "Superb",
+                    Color = "Orange",
+                    Date = new DateTime(2000, 10, 22),
+                    ListPrice = 3.00,
+                    ProductNumber = "2bornot2b",
+                    SellStartDate = new DateTime(2000, 10, 22),
+                    SafetyStockLevel = 3,
+                    DaysToManufacture = 4,
+                    FinishedGoodsFlag = true,
+                    ModifiedDate = new DateTime(2000, 10, 22),
+                    MakeFlag = false,
+                    ReorderPoint = 77,
+                    StandardCost = 5,
+                });
+
+                repo.UpdateProduct(new Product { ProductId = 1, Name = "Superb" });
+                Console.ReadLine();
             }
 
-            repo.DeleteProduct(500);
-
-            repo.InsertProduct(new Product
-            {
-                Name = "Superb",
-                Color = "Orange",
-                Date = new DateTime(2000, 10, 22),
-                ListPrice = 3.00,
-                ProductNumber = "2bornot2b",
-                SellStartDate = new DateTime(2000, 10, 22),
-                SafetyStockLevel = 3,
-                DaysToManufacture = 4,
-                FinishedGoodsFlag = true,
-                ModifiedDate = new DateTime(2000, 10, 22),
-                MakeFlag = false,
-                ReorderPoint = 77,
-                StandardCost = 5,
-            });
-
-            repo.UpdateProduct(new Product { ProductId = 1, Name = "Superb" });
-            Console.ReadLine();
+            
         }
 
 
